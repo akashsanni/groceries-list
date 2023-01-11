93bea3d2c677470d6fe51663cb079b96ffde1ae4
Akash kumar <akash8825317696@gmail.com>
2023-01-11 23:59:23 +0530

    commit 1

---
+++ app.js
@@ -0,0 +1,24 @@
+// ****** SELECT ITEMS **********
+// const submitBtn = document.querySelector('.submit-btn')
+
+// form.addEventListener('submit',)
+
+// submitBtn.addEventListener("submit",function(e){
+//     e.preventDefault()
+// })
+// edit option
+
+// ****** EVENT LISTENERS **********
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
+
+
+
+const clearBtn = document.querySelector('.clear-btn')
+clearBtn.addEventListener('click',function(){
+ document.querySelector('.grocery-container').classList.remove('grocery-list')
+})
